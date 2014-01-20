5e3af18b4f8c5573dc1595ee2b28cf304729d2b6
Artur Dorda <artur.dorda@schibsted.pl>
2014-01-20 23:05:56 +0100

    Changes in DB structure

--- sql/000_create_tables.sql
+++ sql/000_create_tables.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS user (
   id SERIAL primary key,
   username varchar(255) NOT NULL,
-  name varchar(255) DEFAULT NULL,
+  first_name varchar(255) DEFAULT NULL,
   surname varchar(255) DEFAULT NULL,
   email varchar(255) NOT NULL,
-  date_of_birth date DEFAULT NULL,
+  birth_date date DEFAULT NULL,
   registered timestamp NOT NULL
 );
 --rollback DROP TABLE user;
@@ -12,20 +12,27 @@ CREATE TABLE IF NOT EXISTS user (
 CREATE TABLE IF NOT EXISTS movie (
   id SERIAL primary key,
   title varchar(255) DEFAULT NULL,
-  surname varchar(255) DEFAULT NULL,
+  director bigint DEFAULT NULL,
   premiere date DEFAULT NULL,
   type varchar(30) NOT NULL,
   reserved boolean DEFAULT FALSE,
-  reserved_by BIGINT DEFAULT NULL,
+  reserved_by bigint DEFAULT NULL,
   reservation_expires timestamp DEFAULT NULL
 );
 --rollback DROP TABLE movie;
+
+CREATE TABLE IF NOT EXISTS director (
+  id SERIAL primary key,
+  name varchar(255) DEFAULT NULL,
+  surname varchar(255) DEFAULT NULL,
+  birth_date date DEFAULT NULL
+);
+--rollback DROP TABLE director;
 
 CREATE TABLE IF NOT EXISTS actor (
   id SERIAL primary key,
   name varchar(255) DEFAULT NULL,
   surname varchar(255) DEFAULT NULL,
-  date_of_birth date DEFAULT NULL
+  birth_date date DEFAULT NULL
 );
 --rollback DROP TABLE actor;
-
